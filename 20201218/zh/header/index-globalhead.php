64ddab93e3abbb765e74c82cbde185496866c6b9
BAOPAD <51699445+baopad@users.noreply.github.com>
2021-06-20 12:53:37 +0800

    Delete 20201218 directory

--- 20201218/zh/header/index-globalhead.php
+++
@@ -1,7 +0,0 @@
-<?php
-//$lang 在目录文件index.php中定义
-// 载入页面头信息
-include $_SERVER[ 'DOCUMENT_ROOT' ] . "/zh/header/globalhead.php";
-// 载入全局导航条
-include $_SERVER[ 'DOCUMENT_ROOT' ] . "/zh/nav/globalnav.php";
-?>
